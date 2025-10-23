028ac120a859dc2dbef36c35bf4e03d1b6c8e8c7
bahram mosadegh <54316573+bahram-mosadegh@users.noreply.github.com>
2025-10-23 12:45:48 +0330

    Create 2025_10_22_074114_add_personal_info_columns_to_adult_children_table.php

---
+++ database/migrations/2025_10_22_074114_add_personal_info_columns_to_adult_children_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddPersonalInfoColumnsToAdultChildrenTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('adult_children', function (Blueprint $table) {
+            $table->json('continents_visited')->nullable()->after('education_degree');
+            $table->boolean('had_foreign_trip')->nullable()->after('education_degree');
+            $table->string('job')->nullable()->after('education_degree');
+            $table->enum('passport_image_status', ['not_selected', 'accepted', 'rejected'])->after('passport_image');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('adult_children', function (Blueprint $table) {
+            $table->dropColumn('continents_visited');
+            $table->dropColumn('had_foreign_trip');
+            $table->dropColumn('job');
+            $table->dropColumn('passport_image_status');
+        });
+    }
+}
