61855bbcac2fbb9df31e30f732ff59357890564f
bahram mosadegh <54316573+bahram-mosadegh@users.noreply.github.com>
2025-10-26 14:28:29 +0330

    Update Child.php

--- app/Models/Child.php
+++ app/Models/Child.php
@@ -9,23 +9,7 @@ class Child extends Model
 {
     use HasFactory;
 
-    protected $fillable = [
-        'applicant_id',
-        'spouse_id',
-        'name',
-        'last_name',
-        'gender',
-        'passport_number',
-        'expire_date',
-        'birth_date_fa',
-        'birth_date_en',
-        'birth_country',
-        'birth_city',
-        'citizenship_country',
-        'passport_image',
-        'face_image',
-        'face_image_status',
-    ];
+    protected $guarded = [];
 
     public function applicant()
     {
